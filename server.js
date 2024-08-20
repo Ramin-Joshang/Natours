a59fc9a019323f0a8b9dabafda0e4508446049e2
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-08-20 12:52:13 +0330

    handle unhanldeRejection error

--- server.js
+++ server.js
@@ -16,11 +16,20 @@ mongoose
     });
 
 
-
-
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on por ${port}...`);
 });
+
+
+process.on("unhandledRejection", err => {
+    console.log(err.name, err.message);
+    console.log("Unhandled rejection? 🧨 Shutting down...");
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
+
+
+
