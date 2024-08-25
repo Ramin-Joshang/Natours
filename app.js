96aeabf19baef084d51254facdc55f142322fd54
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-08-25 12:12:32 +0330

    Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss');
+const hpp = require('hpp');
 const AppError = require('./utils/appError')
 const globalErrorHandler = require("./controllers/errorController")
 const tourRouter = require("./routes/tourRoutes")
@@ -29,6 +30,17 @@ app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+app.use(hpp({
+    whitelist: [
+        "duration",
+        "ratingsQuantity",
+        "ratingsAverage",
+        "maxGroupSize",
+        "difficulty",
+        "price"
+    ]
+}))
 
 // Data
 
