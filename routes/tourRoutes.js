3e9dfe2444c6074380ee1b56fea9cc6b6f267c0b
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-08-27 12:56:01 +0330

    add Get nested endpoint on reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const { getAllTours, createTour, getTourById, updateTour, deleteTour, checkId, checkBody, aliasTopTours, getTourStats, getMonthlyPlan } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
-const { createReview } = require('../controllers/reviewController');
+// const { createReview } = require('../controllers/reviewController');
+const reviewRouter = require("./reviewRoutes")
 
 const router = express.Router();
 
 // router.param("id", checkId);
+
+// * POST /tour/234fad4/reviews
+// * GET /tour/234fad4/reviews
+// * GET /tour/234fad4/reviews/94887fda
+
+// router.route("/:tourId/reviews")
+//     .post(protect, restrictTo("user"), createReview)
+
+router
+    .use("/:tourId/reviews", reviewRouter)
 
 router.
     route("/top-5-cheap")
@@ -30,11 +41,6 @@ router
     .patch(updateTour)
     .delete(protect, restrictTo("admin", "lead-guide"), deleteTour)
 
-// * POST /tour/234fad4/reviews
-// * GET /tour/234fad4/reviews
-// * GET /tour/234fad4/reviews/94887fda
 
-router.route("/:tourId/reviews")
-    .post(protect, restrictTo("user"), createReview)
 
 module.exports = router;
